d485f202a356f2458eba6f19d3fd95ffdb5f3b71
Nicolò Diamante <hello@nicolodiamante.com>
2023-08-16 13:56:15 +0200

    improved Nodo installation process, added gitattributes

--- utils/install.sh
+++ utils/install.sh
@@ -1,24 +1,27 @@
-#!/bin/sh
+#!/bin/zsh
 
 #
-# Install nodo
+# Install Nodo
 #
 
-# Determines the current user's shell, if `zsh` then installs.
-[[ "$(basename -- "$SHELL")" == "zsh" ]] || exit 1
+# Determines the current user's shell, if not `zsh` then exit.
+if [[ "$(basename -- "$SHELL")" != "zsh" ]]; then
+  echo "Please switch to zsh shell to continue."
+  exit 1
+fi
 
 # Defines the PATHs.
 SCRIPT="${HOME}/nodo/script"
 ZSHRC="${ZDOTDIR:-${XDG_CONFIG_HOME/zsh:-$HOME}}/.zshrc"
 
 if [[ -d "$SCRIPT" && -f "$ZSHRC" ]]; then
-  # Appends to `zshrc`.
-  cat << EOF >> ${ZSHRC}
+# Append the necessary lines to zshrc.
+cat << EOF >> ${ZSHRC}
 # nodo
 fpath=(~/nodo/script \$fpath)
 autoload -Uz nodo
 EOF
-  echo 'zsh: appended nodo to zshrc.'
+  echo 'zsh: appended Nodo's necessary lines to .zshrc'
 
   # Reloads shell.
   source "${ZSHRC}"
